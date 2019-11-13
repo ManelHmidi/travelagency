1703c9e3c01bce62d6666975a5804eac924fdf55
manel <hmidimenel.1@gmail.com>
2019-11-13 23:31:45 +0100

    delete and update user by ID

--- src/main/java/com/ditraacademy/travelagency/core/controller.java
+++ src/main/java/com/ditraacademy/travelagency/core/controller.java
@@ -1,10 +1,7 @@
 package com.ditraacademy.travelagency.core;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,14 +13,27 @@ public class controller {
 
     @PostMapping("/user")
     public void createUser (@RequestBody User user){
-      User user1 = new User();
-      user1.setName("manel");
         userRepository.save(user);
     }
 
     @GetMapping ("/users")
     public List<User> getUsers (){
+
         return userRepository.findAll();
+    }
+//method delete user from the database
+    @DeleteMapping("/users/{id}")
+    public void DeleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+    }
+
+    //method update user by ID
+    @PutMapping("/users/{id}/{name}/{age}")
+     public void UpdateUserById(@PathVariable int id, @PathVariable String name, @PathVariable int age){
+       User user=userRepository.getOne(id);
+       user.setName(name);
+       user.setAge(age);
+       userRepository.save(user);
     }
 
 }
